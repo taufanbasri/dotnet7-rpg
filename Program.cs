130e4c321d75dbd660346c142485155d1441b30e
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-24 23:24:15 +0700

    Create Character Services

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
