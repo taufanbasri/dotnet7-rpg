1f3b2a0348a0e20a0a7dcd63ef5d1f01cde79f7c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-05-26 17:24:51 +0700

    Update Character with AutoMapper

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -69,12 +69,7 @@ namespace dotnet_rpg.Services.CharacterService
                     throw new Exception($"Character with Id '{updateCharacter.Id}' not found.");
                 }
 
-                character.Name = updateCharacter.Name;
-                character.HitPoints = updateCharacter.HitPoints;
-                character.Strength = updateCharacter.Strength;
-                character.Defense = updateCharacter.Defense;
-                character.Intelligence = updateCharacter.Intelligence;
-                character.Class = updateCharacter.Class;
+                _mapper.Map(updateCharacter, character);
 
                 serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
             }
